df5b9810b03c1cf7e2f84ea51401928b18398601
maririaaa <maririaa4322@gmail.com>
2018-12-12 10:44:59 +0900

    181212_1

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,56 @@
 $(".main>.fa").click(function () {
    $("#back").stop().slideToggle(300);
  });
+
+
+$(".port").click(function(){
+    $(".site").stop().slideToggle(300);
+ });
+
+
+ $("#bar").click(function(){
+    $("#bar").hide();
+    $("#close").show().click(function(){
+      $("#bar").show();
+      $("#close").hide();
+    });
+ });
+
+
+ 
+/****사이트 호버 */
+
+
+$(".site>a:first-child").hover(function(){
+   $("#novo").show()
+}, function(){
+    $("#novo").hide()
+});
+
+
+$(".site>a:nth-child(2)").hover(function(){
+    $("#kim").show()
+ }, function(){
+     $("#kim").hide()
+ });
+
+ 
+$(".site>a:nth-child(3)").hover(function(){
+    $("#toms").show()
+ }, function(){
+     $("#toms").hide()
+ });
+
+ 
+$(".site>a:nth-child(4)").hover(function(){
+    $("#ns").show()
+ }, function(){
+     $("#ns").hide()
+ });
+
+ 
+$(".site>a:nth-child(5)").hover(function(){
+    $("#guri").show()
+ }, function(){
+     $("#guri").hide()
+ });
--- public/js/main1.js
+++ public/js/main1.js
@@ -1,4 +1,5 @@
 
+/*
 $(".xs").mousemove(function(evt){
     var delta = 30;
     var cY = evt.clientY;
@@ -6,6 +7,81 @@ $(".xs").mousemove(function(evt){
     var mY = (iY - cY)/delta;
     $(this).find(".phone").css("transform","translate("+mY+"px)")
 });
+*/
+
+var meChk = false;
+
+$("#me").click(function(){
+    if(!meChk) {
+        $(".xs").stop().animate({"left":"20%"},500, "linear", function(){
+            $(this).stop().animate({"top":"-5%"}, 1500, "swing");
+            $("#cont_about").stop().animate({"top":"9%"}, 1500, "linear");
+            meChk = true;
+        });
+    }
+    else {
+        $("#cont_skill").stop().css({"top":"110%"});
+        $("#cont_about").stop().animate({"top":"9%"}, 1500, "linear");
+    }
+});
+
+$("#skill").click(function(){
+    if(!meChk) {
+        $(".xs").stop().animate({"left":"20%"},500, "linear", function(){
+            $(this).stop().animate({"top":"-5%"}, 1500, "swing");
+            $("#cont_skill").stop().animate({"top":"9%"}, 1500, "linear");
+            meChk = true;
+        });
+    }
+    else {
+        $("#cont_about").stop().css({"top":"110%"});
+        $("#cont_skill").stop().animate({"top":"9%"}, 1500, "linear");
+    }
+});
+/****사이트 호버 */
+$(".site>a").hover(function(){
+   $(".xs").hide()
+}, function(){
+    $(".content").hide()
+});
+
+$(".port").hover(function(){
+    $("#bgs").show()
+}, function(){
+    $("#bgs").hide()
+});
 
 
+$(".site>a:first-child").hover(function(){
+   $("#novo").show()
+}, function(){
+    $("#novo").hide()
+});
 
+
+$(".site>a:nth-child(2)").hover(function(){
+    $("#kim").show()
+ }, function(){
+     $("#kim").hide()
+ });
+
+ 
+$(".site>a:nth-child(3)").hover(function(){
+    $("#toms").show()
+ }, function(){
+     $("#toms").hide()
+ });
+
+ 
+$(".site>a:nth-child(4)").hover(function(){
+    $("#ns").show()
+ }, function(){
+     $("#ns").hide()
+ });
+
+ 
+$(".site>a:nth-child(5)").hover(function(){
+    $("#guri").show()
+ }, function(){
+     $("#guri").hide()
+ });
